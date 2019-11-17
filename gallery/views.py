9da7b452667a314e071b052e04a779d061d487df
Flavius Stroe <stroe.flavius@gmail.com>
2019-11-17 20:21:25 +0000

    add pagination to media gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger
 from gallery.apps import S3BucketUtility
 
 # Create your views here
 
 
 def gallery(request):
+
+    page_gallery = Paginator(S3BucketUtility().s3_images_array(), 28)
+    page = request.GET.get('page', 1)
+
+    try:
+        images = page_gallery.page(page)
+    
+    except PageNotAnInteger:
+        images = page_gallery.page(1)
+
     return render(request,
                   'gallery.html',
-                  {'s3_images_array': S3BucketUtility().s3_images_array()},
+                  {'s3_images': images},
                   )
