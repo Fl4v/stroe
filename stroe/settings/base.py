78cc150f22a553f2845100ebe40ab22da03ec457
Flavius Stroe <stroe.flavius@gmail.com>
2019-11-21 23:19:08 +0000

    amending secret key

--- stroe/settings/base.py
+++ stroe/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['STROE_DJANGO_SECRET_KEY']
+SECRET_KEY = 'qx@_h1o#9t27ehz+f8izk*yrdd#dgau04#h4lzys!6-sft1n2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
