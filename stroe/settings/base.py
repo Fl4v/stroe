bbc0c5a0c94f5062c3050d4390ed1e0bc3012b47
Flavius Stroe <stroe.flavius@gmail.com>
2019-11-21 13:29:30 +0000

    Fixing issue with IP and amending homepage content

--- stroe/settings/base.py
+++ stroe/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['STROE_DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'stroe.co.uk', '35.178.117.143']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
